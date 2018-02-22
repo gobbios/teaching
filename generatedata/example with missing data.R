d6e885632e3211791d5dfb0782cadb3571e7ab34
Christof Neumann <christofneumann1@gmail.com>
2018-02-22 14:31:54 +0900

    added third predictor to exampledata01

--- generatedata/example with missing data.R
+++ generatedata/example with missing data.R
@@ -1,9 +1,11 @@
 set.seed(123)
 pred1 <- round(rnorm(n = 500), 2)
 pred2 <- round(rnorm(n = 500), 2)
+pred3 <- round(rnorm(n = 500), 2)
 resp <-  round(1.5 * pred1 - 1 * pred2 + rnorm(n = 500, sd = 10), 2)
-xdata <- data.frame(pred1, pred2, resp)
+xdata <- data.frame(pred1, pred2, pred3, resp)
 xdata$pred1[sample(1:length(pred1), 3)] <- NA
 xdata$pred2[sample(1:length(pred2), 2)] <- NA
+xdata$pred3[sample(200:length(pred3), 50)] <- NA
 
 write.table(xdata, file="data_files/exampledata01.txt", sep = "\t", row.names = FALSE, quote = FALSE)
